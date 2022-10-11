feaab05d11398ff7164a98ae5dd1b6fecd514f74
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-11 23:06:49 +0900

    [new] 주어진 숫자가 몇 자리 수인지 출력하는 프로그램

---
+++ src/codeup/DigitNumbers.java
@@ -0,0 +1,20 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class DigitNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(">>> ");
+        int input = sc.nextInt();
+
+        int cnt = 0;
+
+        while (input != 0) { // input = 4322 -> input = 432 -> input 43 -> input 4
+            input /= 10;
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
