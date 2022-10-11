de77e1c2a90678e9cf69001cf2c70701be2dce9e
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-11 22:59:51 +0900

    [update] 주석 내용 추가 / 변경

--- src/programmers/Summation.java
+++ src/programmers/Summation.java
@@ -7,11 +7,11 @@ public class Summation {
         Scanner sc = new Scanner(System.in);
 
         int input = sc.nextInt();
-        int sum = 0;
+        int sum = 0; // 각 자릿수 숫자들의 합을 저장하는 정수형 변수
 
-        while (input != 0) {
-            sum += input % 10; // sum = 123 % 10 = 3
-            input /= 10; // 123 / 10 = 12
+        while (input != 0) { // 10으로 나누었을 때 몫이 0이 되기 전까지 (즉, 모든 각 자릿수들의 숫자들을 찾아서 더한 후)
+            sum += input % 10;
+            input /= 10;
         }
     }
 }
