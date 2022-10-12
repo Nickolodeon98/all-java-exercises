1de91786e856a74cb7af0b1e9f7378d5d1037715
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-13 00:01:03 +0900

    [new] Hospital 파일을 읽어서 필요한 정보를 추출해 SQL 스크립트를 생성하는 프로그램

---
+++ src/week4/DB/CreateHospitalCsvMain.java
@@ -0,0 +1,12 @@
+package week4.DB;
+
+import java.io.IOException;
+
+public class CreateHospitalCsvMain {
+    public static void main(String[] args) throws IOException {
+        String filename = "C:\\LikeLion\\2022.10\\221011\\서울시 병의원 위치 정보.csv";
+        CreateHospitalCsv createHospitalCsv = new CreateHospitalCsv(filename);
+
+        createHospitalCsv.writeASqlFile("./testScript.sql");
+    }
+}
