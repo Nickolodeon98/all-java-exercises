47fd81a8e95460d08be2c4b068135a9eb7231a54
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-14 00:00:37 +0900

    [update] 백준 3018번 캠프파이어 문제 CampFire 클래스 생성자 및 멤버 메소드 knowSongs 추가

--- src/week4/baekjoon/CampFire.java
+++ src/week4/baekjoon/CampFire.java
@@ -1,9 +1,34 @@
 package week4.baekjoon;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CampFire {
-    private HashMap<Integer, Integer> songAwareness = new HashMap<>();
+    private HashMap<Integer, Integer> songAwareness;
 
+    private boolean sunExists;
 
+    public CampFire(HashMap<Integer, Integer> songAwareness) {
+        this.songAwareness = songAwareness;
+        this.sunExists = false;
+    }
+
+    public void knowSongs(String line) {
+        String[] participants = line.split(" ");
+
+        for (String participant : participants) {
+            if (Integer.parseInt(participant) == 1) sunExists = true;
+            if (songAwareness.get(Integer.parseInt(participant)) != null) {
+                songAwareness.put(Integer.parseInt(participant), songAwareness.get(Integer.parseInt(participant)) + 1);
+            }
+        }
+
+        if (sunExists) {
+
+            for (Map.Entry eachLearnSong : songAwareness.entrySet()) {
+
+                System.out.println(eachLearnSong.getValue().toString());
+            }
+        }
+    }
 }
