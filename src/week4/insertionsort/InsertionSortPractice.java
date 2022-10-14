9435b43022e84f2752679979362f0b19931b8992
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-14 09:47:53 +0900

    [update] InsertionSortPractice 클래스 내에서 swap 구현

--- src/week4/insertionsort/InsertionSortPractice.java
+++ src/week4/insertionsort/InsertionSortPractice.java
@@ -1,10 +1,15 @@
 package week4.insertionsort;
 
 public class InsertionSortPractice {
-
     public int[] sort(int[] arr) {
         int i = 1;
 
-        return new int[]{};
+        if (arr[i] < arr[i-1]) {
+            int temp = arr[i];
+            arr[i] = arr[i-1];
+            arr[i-1] = temp;
+        }
+
+        return arr;
     }
 }
