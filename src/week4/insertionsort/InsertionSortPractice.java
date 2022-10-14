adbb16fcb439b29fde29d5e414a5a79e1704dab7
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-14 09:37:44 +0900

    [new] 삽입정렬을 위한 InsertionSortPractice 클래스 생성

---
+++ src/week4/insertionsort/InsertionSortPractice.java
@@ -0,0 +1,10 @@
+package week4.insertionsort;
+
+public class InsertionSortPractice {
+
+    public int[] sort(int[] arr) {
+        int i = 1;
+
+        return new int[]{};
+    }
+}
---
+++ src/week4/insertionsort/InsertionSortPracticeMain.java
@@ -0,0 +1,14 @@
+package week4.insertionsort;
+
+import java.util.Arrays;
+
+public class InsertionSortPracticeMain {
+    public static void main(String[] args) {
+        int[] arr = {8, 5, 6, 2, 4};
+
+        InsertionSortPractice insertionSortPractice = new InsertionSortPractice();
+        int[] resultArr = insertionSortPractice.sort(arr);
+
+        System.out.println(Arrays.toString(resultArr));
+    }
+}
