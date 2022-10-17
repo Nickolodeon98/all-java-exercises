af75bdf3462e0d5b521cc510084beb2aa87f9984
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-17 09:07:53 +0900

    [new] 직각삼각형 별 찍기

---
+++ src/week5/StarRightTriangle.java
@@ -0,0 +1,10 @@
+package week5;
+
+public class StarRightTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+    }
+}
