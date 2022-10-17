3a8c63a9b0c7ccec251086d579fcc0bc1115b01d
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-17 09:23:41 +0900

    [update] for 문 사용하여 직각삼각형 별 찍기

--- src/week5/StarRightTriangle.java
+++ src/week5/StarRightTriangle.java
@@ -1,10 +1,26 @@
 package week5;
 
+import java.util.Scanner;
+
 public class StarRightTriangle {
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("* *");
-        System.out.println("* * *");
-        System.out.println("* * * *");
+//        System.out.println("*");
+//        System.out.println("* *");
+//        System.out.println("* * *");
+//        System.out.println("* * * *");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print(">>> ");
+        int n = sc.nextInt();
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
